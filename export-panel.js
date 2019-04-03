a3a5999ebaec23694ce64e3e34ab7371e0e95f45
Pawel Psztyc <jarrodek@gmail.com>
2019-04-02 22:48:34 -0700

    Update: Updating import paths to match web spec

--- export-panel.js
+++ export-panel.js
@@ -11,10 +11,10 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import {PolymerElement} from '@polymer/polymer/polymer-element.js';
-import '@polymer/paper-styles/shadow.js';
+import {PolymerElement} from '../../@polymer/polymer/polymer-element.js';
+import '../../@polymer/paper-styles/shadow.js';
 import './export-form.js';
-import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+import {html} from '../../@polymer/polymer/lib/utils/html-tag.js';
 /**
  * Data export panel for Advanced REST Client.
  *
